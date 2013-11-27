117b54db4f4ce3b9ff71f99572ed30fab93be2db
maruyama <norihiro.maruyama@gmail.com>
2013-11-27 16:23:48 +0900

    no message

--- robot/utils/largechar2smallchar.sh
+++
@@ -1,5 +0,0 @@
-mv ../../scope/scope.C ../../scope/scope_main.C
-perl -p -i.bak -e 's/scope.C/scope_main.C/g' ../../scope/Makefile.am
-
-mv ../../neurosock/neurosock.C ../../neurosock/neurosock_main.C
-perl -p -i.bak -e 's/neurosock.C/neurosock_main.C/g' ../../neurosock/Makefile.am
