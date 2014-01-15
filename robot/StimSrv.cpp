1681bee328c65c7b72d98e4f3db670e061a0f8c0
maruyama <norihiro.maruyama@gmail.com>
2014-01-15 15:33:13 +0900

    add StimSrv.

---
+++ robot/StimSrv.cpp
@@ -0,0 +1,64 @@
+
+#include "StimSrv.h"
+
+StimSrv::StimSrv()
+{
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_DETACHED);
+    pthread_mutex_init(&mtx, NULL);
+    threadData.deq = &deq;
+    threadData.mtx = &mtx;
+}
+
+StimSrv::~StimSrv(){
+    //closeServer();
+    //deq.clear();
+//    pthread_cancel(thread);
+//    pthread_mutex_destroy(&mtx);
+}
+
+void *StimSrv::thread_func(void *arg){
+    
+    threadData_t *threadData = (threadData_t*)arg;
+    deque<StimData> *pDeque = threadData->deq;
+    //char stmsg[128];
+    
+    while(true){
+        if(pDeque->size()>0){
+            pthread_mutex_lock(threadData->mtx);
+            StimData *stimData = &pDeque->front();
+            send(threadData->fpga_sock, &stimData->stimulation, sizeof(stimData->stimulation), 0);
+            //memset(&stmsg, 0, sizeof(stmsg));
+            //recv(threadData->fpga_sock, stmsg, sizeof(stmsg), 0);
+            //fprintf(stderr,"Stimulation reply :: %i\n",(stmsg[0]<<8) + stmsg[1]);
+            //cout << stimData->stimulation[2] << "\n";
+            pDeque->pop_front();
+            pthread_mutex_unlock(threadData->mtx);
+            usleep(STIM_INTERVAL);
+        }
+   }
+}
+
+void StimSrv::connectServer(){
+    threadData.fpga_sock = connect_server(STIM_TCP_ADDR, STIM_TCP_PORT);
+}
+
+void StimSrv::closeServer(){
+    close_server(threadData.fpga_sock);
+}
+
+void StimSrv::setup(){
+    connectServer();
+    pthread_create(&thread, &attr, thread_func, (void *)&threadData);
+    //pthread_join( thread, NULL );
+}
+
+void StimSrv::sendStim(int dac, int channel){
+    StimData sData;
+    sData.stimulation[1] = htons(dac);
+    sData.stimulation[2] = htons(channel);
+    
+    pthread_mutex_lock(threadData.mtx);
+    threadData.deq->push_back(sData);
+    pthread_mutex_unlock(threadData.mtx);
+}
---
+++ robot/StimSrv.h
@@ -0,0 +1,81 @@
+
+#ifndef ____StimSrv__
+#define ____StimSrv__
+
+#include <iostream>
+#include <unistd.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <deque>
+#include <common/CMOSServerTools.H>
+#include <common/CMOSTools.H>
+
+#define STIM_INTERVAL 10000 //micro second
+
+class StimSrv
+{
+public:
+    StimSrv();
+    ~StimSrv();
+    void connectServer();
+    void closeServer();
+    void setup();
+    void sendStim(int dac, int channel);
+
+private:
+    static void *thread_func(void *);
+    
+    class StimData
+	{
+    private:
+        // experiment variables
+        int chipaddress;// "slot" where chip is plugged [0-4]
+        int dacselection;// 0->DAC1  1->DAC2
+        int channel;// channel ID to stim
+        int volt;// [+/-mV; millivolt]   [current: 0 to 450 allowed -->> 0 to +/-4.12uA ONLY! for low current mode]
+        int stim_mode;// [previous==0; voltage==1; current==2]
+        int phase;// [us; microseconds]
+        int delay;// [0->10,000; milliseconds] delay until stimulating sent to fpga (can change limits in test.c)
+        int epoch;// [0->512] for high res DAC encoding          was [0->127] for low res
+        
+	public:
+        uint16_t stimulation[9];
+        
+        StimData(){
+            //default setup of experiment variables
+            chipaddress         =    4;     // "slot" where chip is plugged [0-4]
+            dacselection        =    0;     // 0->DAC1  1->DAC2
+            channel             =  127;     // No stim by default
+            volt                = 1000;     // [+/-mV; millivolt]   [current: 0 to 450 allowed -->> 0 to +/-4.12uA ONLY! for low current mode]
+            stim_mode           =    1;     // [previous==0; voltage==1; current==2]
+            phase               =  100;    	// [us; microseconds]
+            delay               =    0;     // [0->10,000; milliseconds] delay until stimulating sent to fpga (can change limits in test.c)
+            epoch               =    0;     // [0->512] for high res DAC encoding          was [0->127] for low res
+            
+            stimulation[0] = htons(chipaddress);
+            stimulation[1] = htons(dacselection);
+            stimulation[2] = htons(channel);
+            stimulation[3] = htons(ceil(volt/2.9));
+            stimulation[4] = htons((ceil(phase/50)));
+            stimulation[5] = htons(0);
+            stimulation[6] = htons(0);
+            stimulation[7] = htons(delay);
+            stimulation[8] = htons(stim_mode);
+        }
+	};
+    
+    typedef struct {
+        deque<StimData> *deq;
+        pthread_mutex_t* mtx;
+        int fpga_sock;
+    }threadData_t;
+    
+    threadData_t threadData;
+    pthread_t thread;
+    pthread_attr_t attr;
+    deque<StimData> deq;
+    pthread_mutex_t mtx;
+};
+
+
+#endif /* defined(____StimSrv__) */
