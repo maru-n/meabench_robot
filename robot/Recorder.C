713d7986b9a3dce213c2e68f205ae291d525bf9e
maruyama <norihiro.maruyama@gmail.com>
2014-01-24 19:12:54 +0900

    test code.

--- robot/Recorder.C
+++ robot/Recorder.C
@@ -40,10 +40,10 @@ Recorder::Recorder(SFCVoid *source0,
     dataServer = new TCPDataServer();
     dataServer->setup(TCP_SERVER_PORT);
     dataServer->startListening();
-    /*
+
     stimSrv = new StimSrv();
     stimSrv->setup();
-    */
+
     receivedDataBuffer = (unsigned char*)malloc(TCP_MAX_MSG_SIZE);
     //####################################
 }
