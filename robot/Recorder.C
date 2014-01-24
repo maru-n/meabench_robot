d63bea2a1dbf98408203de9749576420dafb1f61
maruyama <norihiro.maruyama@gmail.com>
2014-01-24 19:06:50 +0900

    test code.

--- robot/Recorder.C
+++ robot/Recorder.C
@@ -85,26 +85,23 @@ timeref_t Recorder::save_some(timeref_t upto) throw(Error)
 
     while (last < end)
     {
-        //printf("last:%Li end:%Li\n",last, end);
         SpikeSFCli *spikeSrc = dynamic_cast<SpikeSFCli *>(source);
         Spikeinfo const &si = (*spikeSrc)[last++];
         //###########################
-        char c = (unsigned char)si.channel;
-        //printf("time:%d channel:%d\n", time(0), c);
         if(dataServer->isConnected()) {
+            char c = (unsigned char)si.channel;
             dataServer->sendRawBytes(&c, 1);
             int receivedSize = dataServer->receiveRawBytes((char*)receivedDataBuffer, TCP_MAX_MSG_SIZE);
             for(int i=0; i<receivedSize; i+=2) {
                 int dacNum = (int)receivedDataBuffer[0];
                 int channelNum = (int)receivedDataBuffer[1];
-                //std::cout << "DAC#" << dacNum << " Channel#" << channelNum << std::endl;
+                std::cout << "DAC#" << dacNum << " Channel#" << channelNum << std::endl;
                 if (dacNum<0 || dacNum>1 || channelNum<0 || channelNum>125) {
                     break;
                 }
                 stimSrv->sendStim(dacNum, channelNum);
             }
         }
-        //printf("last:%Li end:%Li\n",last, end);
         //###########################
     }
     return oldest;
